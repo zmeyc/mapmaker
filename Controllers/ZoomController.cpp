809f74ff2cf917b1b490d4249782908922b7b797
Andrey Fidrya <af@zabiyaka.com>
2016-04-02 03:40:08 +0300

    Add more zoom levels

--- Controllers/ZoomController.cpp
+++ Controllers/ZoomController.cpp
@@ -4,7 +4,26 @@
 #include "Utils/Utils.h"
 
 const double zoomLevels[] = {
-    1.0, 2.0, 3.0, 4.0
+    0.05,
+    0.25 / 4.0,
+    1.0 / 12.0,
+    0.25 / 2.0,
+    1.0 / 6.0,
+    0.25,
+    1.0 / 3.0,
+    0.5,
+    2.0 / 3.0,
+    1.0,
+    2.0,
+    3.0,
+    4.0,
+    5.0,
+    6.0,
+    7.0,
+    8.0,
+    12.0,
+    16.0,
+    32.0
 };
 const size_t zoomLevelsCount = sizeof(zoomLevels) / sizeof(zoomLevels[0]);
 
